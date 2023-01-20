2bc63b3b6c583dcc8f72e81d64bc471f0c8395df
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-20 16:58:27 +0300

    HomeWork8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,19 @@
+﻿
+void ListEven(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            System.Console.Write($"{i} ");
+        }
+    }
+}
+int N;
+System.Console.Write("Введите число N: ");
+while(!Int32.TryParse(Console.ReadLine(), out N))
+{
+    System.Console.Write("Необходимо ввести целое число! Пропробуйте ещё раз: ");
+}
+
+ListEven(N);
