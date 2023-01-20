6878d5d1f69a26e2e42ae7c4994d09a8b486e885
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-20 16:58:04 +0300

    HomeWork6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int numb;
+System.Console.Write("Введите целое число: ");
+while(!Int32.TryParse(Console.ReadLine(), out numb))
+{
+    System.Console.Write("Некорректный ввод! Введите целое число: ");
+}
+if(numb % 2 == 0)
+{
+    System.Console.WriteLine("Чётное");
+}
+else
+{
+    System.Console.WriteLine("Нечётное");
+}
